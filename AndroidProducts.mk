8f7d22cff61e24c101037ac1c6fa5b2c71ba2a1f
ripee <ripee.dev@gmail.com>
2019-12-22 11:49:17 +0100

    nobleltetmo: Renouveau-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/rr_nobleltetmo.mk
+    $(LOCAL_DIR)/renouveau_nobleltetmo.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo rr_nobleltetmo-userdebug
+add_lunch_combo renouveau_nobleltetmo-userdebug
