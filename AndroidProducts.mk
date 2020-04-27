2af56620ac8e28b9f82af206cb241f3f32ea2154
manikar <42275875+manikar@users.noreply.github.com>
2020-04-27 14:06:54 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_nobleltebmc.mk
+    
+COMMON_LUNCH_CHOICES := \
+    lineage_nobleltebmc-user \
+    lineage_nobleltebmc-userdebug \
+    lineage_nobleltebmc-eng
